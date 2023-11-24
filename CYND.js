7c697fc3b72b5d994b8ce5f3d240955a6d85bb66
LuxuryLee99 <113966524+LuxuryLee99@users.noreply.github.com>
2023-11-25 01:24:40 +0800

    Update CYND.js

--- CYND.js
+++ CYND.js
@@ -6,14 +6,7 @@
 
 hostname = yqsb.qau.edu.cn
 
-*************************************
-
-var LEE = JSON.parse($response.body);
-
-// 使用正则表达式修改内容
-LEE.someProperty = LEE.someProperty.replace(/"data":".*?\"/g, ""data":{     "nickname" : "李茶青",     "admission_time" : "2023-10-01 ~ 2023-11-30"   }");
-LEE.anotherProperty = LEE.anotherProperty.replace(/"msg":".*?\"/g, ""msg":"登记成功"");
-LEE.thirdProperty = LEE.thirdProperty.replace(/"code":\d+/g, ""code":1");
-
-// 返回修改后的 JSON 响应
-$done({ body: JSON.stringify(LEE) });
+var body = $response.body;
+var obj = JSON.parse(body);
+obj.code = 1;
+$done({ body: JSON.stringify(obj) });

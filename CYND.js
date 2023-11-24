9fe1d75eca66dad07c562b7dfc87b2d9830d49c7
LuxuryLee99 <113966524+LuxuryLee99@users.noreply.github.com>
2023-11-25 01:42:32 +0800

    Update CYND.js

--- CYND.js
+++ CYND.js
@@ -8,5 +8,7 @@ hostname = yqsb.qau.edu.cn
 
 var body = $response.body;
 var obj = JSON.parse(body);
-obj.code = 1;
+obj."code" = 1;
+obj."data" = {     "nickname" : "李茶青",     "admission_time" : "2023-10-01 ~ 2023-11-30"   };
+obj."msg" = "登记成功";
 $done({ body: JSON.stringify(obj) });
